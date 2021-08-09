72fbb9dc763e8dda8ec82aab8c24bd6319b6b920
Gustavo Omonte Acevedo <GustavoGOAweb@gmail.com>
2021-08-09 12:58:19 -0400

    animated Menu

--- app/js/app.js
+++ app/js/app.js
@@ -1,5 +1,19 @@
 const btnHamburger= document.querySelector('#btnHamburger');
+const header = document.querySelector('.header');
+const overlay = document.querySelector('.overlay');
+
+
 btnHamburger.addEventListener('click',function(){
     console.log(`open Hamburger`);
-    btnHamburger.classList.toggle('open');
+    if(header.classList.contains('open')){
+        header.classList.remove('open');
+        overlay.classList.remove('fade-in');
+        overlay.classList.add('fade-out');
+    }
+    else{
+        header.classList.add('open');
+        overlay.classList.add('fade-in');
+        overlay.classList.remove('fade-out');
+    }
+
 });
